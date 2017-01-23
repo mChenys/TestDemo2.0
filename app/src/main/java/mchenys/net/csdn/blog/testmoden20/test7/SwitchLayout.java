8637da12229afb27307c4727e7b264850a8cfa9d
mChenys <643353964@qq.com>
2017-01-23 23:26:56 +0800

    add

---
+++ app/src/main/java/mchenys/net/csdn/blog/testmoden20/test7/SwitchLayout.java
@@ -0,0 +1,128 @@
+package mchenys.net.csdn.blog.testmoden20.test7;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * Created by mChenys on 2017/1/23.
+ */
+public class SwitchLayout extends FrameLayout {
+    private static final String TAG = "SwitchLayout";
+    private View mLeftTv;
+    private View mRightTv;
+    private float mViewPadding;
+    private int mSwitchWidth;
+    private OnSwitchCallback mOnSwitchCallback;
+
+    public interface OnSwitchCallback{
+        //切换到左边
+        void onLeft();
+        //切换到右边
+        void onRight();
+    }
+
+    public void setOnSwitchCallback(OnSwitchCallback c) {
+        mOnSwitchCallback = c;
+    }
+    public SwitchLayout(Context context) {
+        this(context, null);
+    }
+
+    public SwitchLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SwitchLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mViewPadding = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
+        setBackgroundColor(Color.RED);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mLeftTv = getChildAt(0);
+        mRightTv = getChildAt(1);
+        mLeftTv.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                turnRight();
+                return false;
+            }
+        });
+        mRightTv.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                turnLeft();
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int halfWidth = (int) (mLeftTv.getMeasuredWidth() + mViewPadding + 0.5f * mRightTv.getMeasuredWidth());
+        int w = 2 * halfWidth;
+        int h = Math.max(mLeftTv.getMeasuredHeight(), mRightTv.getMeasuredHeight());
+        setMeasuredDimension(w, h);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        int lTop = (int) (0.5f * (getHeight() - mLeftTv.getHeight()));
+        int rTop = (int) (0.5f * (getHeight() - mRightTv.getHeight()));
+        mLeftTv.layout(0, lTop, mLeftTv.getWidth(), lTop + mLeftTv.getHeight());
+        int rLeft = (int) (mLeftTv.getRight() + mViewPadding);
+        mRightTv.layout(rLeft, rTop, rLeft + mRightTv.getWidth(), rTop + mRightTv.getHeight());
+        mSwitchWidth = (int) (0.5f * (getWidth() - mLeftTv.getWidth()));
+    }
+
+    public void turnLeft() {
+        if (getScrollX() != 0) {
+            scrollBy(-getScrollX(), 0);
+            if (null != mOnSwitchCallback) {
+                mOnSwitchCallback.onRight();
+            }
+        }
+    }
+
+    public void turnRight() {
+        if (getScrollX() != -mSwitchWidth) {
+            scrollBy(-mSwitchWidth, 0);
+            if (null != mOnSwitchCallback) {
+                mOnSwitchCallback.onLeft();
+            }
+        }
+    }
+
+    private int lastX;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) event.getRawX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = (int) (event.getRawX() - lastX);
+                // 右滑
+                if (deltaX > 50) {
+                    turnRight();
+                }
+                // 左滑
+                else if (deltaX < -50) {
+                    turnLeft();
+                }
+
+        }
+
+        return true;
+    }
+}
--- app/src/main/java/mchenys/net/csdn/blog/testmoden20/test7/TestRecordViewActivity.java
+++ app/src/main/java/mchenys/net/csdn/blog/testmoden20/test7/TestRecordViewActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import mchenys.net.csdn.blog.testmoden20.R;
 
@@ -16,6 +18,7 @@ public class TestRecordViewActivity extends AppCompatActivity {
     private RecordView mRecordView;
     private boolean isRecord;
     private TextView mTimeTv;
+    private SwitchLayout mSwitchLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,6 +27,18 @@ public class TestRecordViewActivity extends AppCompatActivity {
         mRecordView = (RecordView) findViewById(R.id.recordView);
         mTimeTv = (TextView) findViewById(R.id.tv_time);
         mRecordView.setMaxDuration(6);
+        mSwitchLayout = (SwitchLayout) findViewById(R.id.switchLayout);
+        mSwitchLayout.setOnSwitchCallback(new SwitchLayout.OnSwitchCallback() {
+            @Override
+            public void onLeft() {
+                Toast.makeText(TestRecordViewActivity.this, "left...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRight() {
+                Toast.makeText(TestRecordViewActivity.this, "right...", Toast.LENGTH_SHORT).show();
+            }
+        });
         mRecordView.setOnRecordCallback(new RecordView.OnRecordCallback() {
             @Override
             public void onFinish() {
@@ -36,7 +51,29 @@ public class TestRecordViewActivity extends AppCompatActivity {
             }
         });
     }
+    private int lastX;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) event.getRawX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = (int) (event.getRawX() - lastX);
+                // 右滑
+                if (deltaX > 50) {
+                    mSwitchLayout.turnRight();
+                }
+                // 左滑
+                else if (deltaX < -50) {
+                    mSwitchLayout.turnLeft();
+                }
+
+        }
+
+        return false;
+    }
     public void takePhoto(View view) {
         mRecordView.startCapture();
         isRecord = false;
